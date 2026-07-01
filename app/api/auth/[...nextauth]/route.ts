f2ca2bf3ff0b9056eaca46ed25833bb467dfc65b
sri venkatesh <srivenkatesh424@gmail.com>
2026-07-01 11:22:32 +0530

    Implement Google authentication with Prisma and Neon

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,3 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
+import { handlers } from "@/auth";
 
-const handler = NextAuth({
-  providers: [
-    Google({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-})
-
-export { handler as GET, handler as POST }
+export const { GET, POST } = handlers;
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,11 +1,13 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as {
+  prisma?: PrismaClient;
+};
 
 export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ["query"],
-  });
+  globalForPrisma.prisma ??
+  new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
